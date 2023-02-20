0acb97b24c4b1be34eed263fb1ff8d4935df7e82
galvisf <galvisf@stanford.edu>
2023-02-20 08:50:48 -0800

    prepare pip

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='frame_postprocess',
-      version='0.1')
+      version='0.1',
+      url='https://github.com/galvisf/frame_postprocess',
+      license='MIT License',
+      author='Francisco Galvis',
+      author_email='galvisf@alumni.stanford.edu',
+      description='frame_postprocess is an open-source python package that facilitates the postprocessing of seismic nonlinear response history analyses (NLRHA) of 2D OpenSees models of moment frames.',
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
+      packages=find_packages(),
+      include_package_data=True,
+      platforms='any',
+       install_requires=[
+             'numpy',
+             'pandas',
+             'h5py',
+             'matplotlib',
+             'scipy',
+             'posixpath',
+             'os',
+      ],
+      classifiers = [
+             'Programming Language :: Python',
+             'Natural Language :: English',
+             'Intended Audience :: Education',
+             'Intended Audience :: Science/Research',
+             'License :: OSI Approved :: MIT License',
+             'Topic :: Scientific/Engineering',
+      ],
+)
